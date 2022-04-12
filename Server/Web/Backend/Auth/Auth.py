60565d49e729cddb48881439630db244ede618f8
Yarobonz <88908955+Yarobonz@users.noreply.github.com>
2022-04-12 13:43:47 -0400

    USERID support or auth

--- Server/Web/Backend/Auth/Auth.py
+++ Server/Web/Backend/Auth/Auth.py
@@ -8,6 +8,7 @@ username=sys.argv[1]
 email=sys.argv[2]
 password=hashlib.sha256(sys.argv[3].encode()).hexdigest()
 token=sys.argv[4]
+userid=sys.argv[5]
 
 
 
@@ -17,7 +18,7 @@ AccountList = AccountFile.readlines()
     # Username[1], 
 for account in AccountList:
     AccountInfo=account.split(":")
-    if(AccountInfo[0].lower()==username.lower() and AccountInfo[4]==password or AccountInfo[3].lower()==email.lower() and AccountInfo[4]==password or token == AccountInfo[5]):
+    if(AccountInfo[0].lower()==userid.lower() and AccountInfo[4]==password or AccountInfo[3].lower()==email.lower() and AccountInfo[4]==password or token == AccountInfo[5] or AccountInfo[1].lower()==username.lower() and AccountInfo[4]==password):
         print(f"Login as {AccountInfo[0]}:{AccountInfo[1]}")
     else:
         pass
