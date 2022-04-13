31db2f7a24a402907dcf4dcd9447df7ecccc7b99
Yarobonz <88908955+Yarobonz@users.noreply.github.com>
2022-04-13 11:26:37 -0400

    Fixed tagging errors and tagging system along with indents

--- Server/Web/Backend/Auth/Register.py
+++ Server/Web/Backend/Auth/Register.py
@@ -50,6 +50,7 @@ def checkPass(password): #Make sure the password is long enough
     return False
 
 def setTag(): #Set the user tag to allow multiple users with the same name
+    tag=0
     LoginsFile = open(AuthFile, 'r')
     LoginsDict = json.load(LoginsFile)
 
@@ -63,10 +64,10 @@ def saveData(uID, uName, uCode, uEmail, uPass, uToken, uTag): #Save the data int
     if (LoginErrors):
         exit()
     else:       
-        usercode = uCode
+        tag = uCode
         LoginsFile = open(AuthFile, "r+")
         data = json.load(LoginsFile)
-        new_user = {f"{usercode}": {"username": uName, "tag": uTag, "email": uEmail, "password": uPass, "id": uID, "token": uToken}}
+        new_user = {f"{tag}": {"username": uName, "tag": uTag, "email": uEmail, "password": uPass, "id": uID, "token": uToken}}
         data.update(new_user)
         LoginsFile.seek(0)
         json.dump(data, LoginsFile, indent=2)
@@ -99,7 +100,7 @@ Combined=str(username)+str(email)+str(password)
 token=hashlib.sha256(Combined.encode()).hexdigest()
 tag=setTag()
 
-for char in (str(username)+str(usercode)):
+for char in (str(username)+str(tag)):
     userid += ord(char)
 
 
@@ -108,7 +109,7 @@ LoginsFile = json.load(LoginsFile)
     
 try:
     if (not emailInUse(email)):
-        saveData(userid, username, usercode, email, password, token, tag)
+        saveData(userid, username, tag, email, password, token, tag)
         print("Register Success")
 except Exception as e:
     print(e)
