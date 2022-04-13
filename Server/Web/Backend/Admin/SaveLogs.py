9b75301760188fae8a034478f875069843fef5b6
Yarobonz <88908955+Yarobonz@users.noreply.github.com>
2022-04-13 12:24:39 -0400

    quick preformnce update

--- Server/Web/Backend/Admin/SaveLogs.py
+++ Server/Web/Backend/Admin/SaveLogs.py
@@ -3,5 +3,7 @@ import datetime
 
 command = str(sys.argv[1])
 
-with open("logs/shell_logs.txt", "a") as log:
+with open("logs/shell_logs.txt", "a+") as log:
     log.write(command+"\n");
+
+log.close()
