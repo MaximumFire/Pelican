6f2e7ecee744ca5ffa279664737cb8b77a0c5bd5
MaximumFire <68478521+MaximumFire@users.noreply.github.com>
2022-04-15 23:42:19 +0100

    fix create server

--- Server/Backend/Server/CreateChannel.py
+++ Server/Backend/Server/CreateChannel.py
@@ -1,3 +1,15 @@
+import os
 import sys
-sys.path.insert(1, '../Auth/')
-from AuthFunction import Authenticate;
+import re
+import random
+import time
+
+
+sys.path.insert(1, "../Backend/Auth/")
+from AuthFunction import Authenticate, AuthenticateReturnID
+
+SERVERS_DIR = "../Backend/Servers/"
+
+servername=sys.argv[1]
+token=sys.argv[2]
+username=sys.argv[3]
--- Server/Backend/Server/CreateServer.py
+++ Server/Backend/Server/CreateServer.py
@@ -4,13 +4,14 @@ import re
 import random
 import time
 
-import sys
-sys.path.insert(1, '../Auth/')
-from AuthFunction import Authenticate;
+sys.path.insert(1, "../Backend/Auth/")
+from AuthFunction import Authenticate, AuthenticateReturnID
 
+SERVERS_DIR = "../Backend/Servers/"
 
 servername=sys.argv[1]
 token=sys.argv[2]
+username=sys.argv[3]
 
 if(Authenticate(token)):
     ServernameInt=0
@@ -22,19 +23,19 @@ if(Authenticate(token)):
 
     serverid=re.sub("[^0-9]", "",str(ServernameInt)+str(usernameint)+str(random.randint(0,9999))+str(time.time()))
 
-    if(os.path.isdir("../../../Servers/"+str(serverid))):
+    if(os.path.isdir(SERVERS_DIR+str(serverid))):
         print("Server Exists, Wait 1 Second and retry")
-        exit
+        exit()
     else:
-        os.mkdir("../../../Servers/"+str(serverid))
+        os.mkdir(SERVERS_DIR+str(serverid))
 
-        with open(f"../../../Servers/{serverid}/ServerInfo.txt", "a+") as f:
-            f.write(f"{serverid}\nUSERNAME HERE\n{servername}")
+        with open(f"{SERVERS_DIR}{serverid}/ServerInfo.txt", "a+") as f:
+            f.write(f"{serverid}\n{username}\n{servername}")
             f.close()
-        with open(f"../../../Servers/{serverid}/BannedUsers.txt", "a+") as f:
+        with open(f"{SERVERS_DIR}{serverid}/BannedUsers.txt", "a+") as f:
             f.write(f"")
             f.close()
-        with open(f"../../../Servers/{serverid}/InvitedUsers.txt", "a+") as f:
+        with open(f"{SERVERS_DIR}{serverid}/InvitedUsers.txt", "a+") as f:
             f.write(f"{AuthenticateReturnID(token)}\n")
             f.close()
 else:
