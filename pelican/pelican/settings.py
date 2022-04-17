6c77672b90201408dc5dc9708cd63ba63027c098
MaximumFire <68478521+MaximumFire@users.noreply.github.com>
2022-04-17 23:30:08 +0100

    fix

--- pelican/pelican/settings.py
+++ pelican/pelican/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-s))mi*3g2ouy@^c%@!)x@zgw)j!v1(+uhjnnjuxpe624&3u2zq'
+SECRET_KEY = 'django-insecure-s))mi*3g2ouy@^c%@!)x@zgw)j!v1(+uhjnnjuxpe624&3u2zq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
